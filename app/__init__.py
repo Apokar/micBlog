d995ef262f3ee4845ee8bbcff1d697d39fc0f7fe
apokar <apokar@163.com>
2017-04-05 13:38:51 +0800

    1

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+app = Flask(__name__)
+
+from app import views
